39a11f870457ce10ce8bcdab9af6befc0e815872
AishwaryaParihar1 <aish7parihar@gmail.com>
2025-06-12 21:14:22 +0530

    first commit

---
+++ src/Config/ThemeConfig.ts
@@ -0,0 +1,16 @@
+ export class ConfigDB {
+  static data = {
+       settings: {
+           layout_type: 'ltr',
+       sidebar: {
+           type: 'horizontal-wrapper',
+           },
+       },
+       color: {
+           primary_color: '#7A70BA', 
+           secondary_color: '#48A3D7', 
+           mix_background_layout: 'dark-sidebar', 
+       },
+   }
+ }
+  export default ConfigDB;
